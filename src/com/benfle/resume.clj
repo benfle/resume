179efd8bf728fd03ddfb0f26f3f448729dd83eb5
Benoît Fleury <me@benfle.com>
2025-01-21 09:04:27 -0500

    Updated resume.

--- src/com/benfle/resume.clj
+++ src/com/benfle/resume.clj
@@ -148,14 +148,7 @@
      [:footer
       [:p (str "Version: " (.format (java.time.LocalDate/now)
                                     (java.time.format.DateTimeFormatter/ofPattern "MMMM YYYY")))]
-      [:p (str "Latest version at: " url)]]
-     [:script
-      "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-44073995-1', 'benfle.com');
-      ga('send', 'pageview');"]]]])
+      [:p (str "Latest version at: " url)]]]]])
 
 (defn checked-sh
   [& args]
